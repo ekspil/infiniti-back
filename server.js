51d26b4354d751ab8de4beadab73421abde12129
Efremov AV <ekspil@yandex.ru>
2022-07-22 10:48:29 +1000

    1.0.23

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ require('dotenv').config()
 const Fastify = require('fastify')
 
 // Instantiate Fastify with some config
+console.log("Start initialisation ...")
 const app = Fastify({
   logger: true,
   pluginTimeout: 120000
 })
 
+console.log("Before register ...")
 // Register your application as a normal plugin.
 app.register(require('./app.js'))
 
+console.log("After register ...")
 // Start listening.
 app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
   if (err) {
